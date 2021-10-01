c0efec571ac7e9f6c4269136801e25c8c1ca4771
new2 <new2@wael>
2021-09-30 21:12:36 -0400

    added part

--- src/Two/DemoTwo.java
+++ src/Two/DemoTwo.java
@@ -3,5 +3,11 @@ package Two;
 public class DemoTwo {
 public static void main (String[]args) {
 	System.out.println("Demo Two.....");
+	boolean isSunday= true;
+	if(isSunday) {
+		System.out.println("abc");
+	}else {
+		System.out.println("Xyz");
+	}
 }
 }
